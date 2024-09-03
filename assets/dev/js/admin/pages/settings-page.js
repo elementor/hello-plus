dd0630b162c48b41254ed901ec011e4bdecdf540
Nurit <nurit@elementor.com>
2024-09-03 17:44:45 +0300

    initial home page [TMZ-05]

--- assets/dev/js/admin/pages/settings-page.js
+++ assets/dev/js/admin/pages/settings-page.js
@@ -1,11 +1,11 @@
-import { useState, useEffect, Fragment } from 'react';
+import { Fragment } from 'react';
 import { store as noticesStore } from '@wordpress/notices';
-import { dispatch, useDispatch, useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import api from '@wordpress/api';
-import { Button, Panel, Placeholder, Spinner, SnackbarList } from '@wordpress/components';
-import { SettingsPanel } from './../panels/settings-panel.js';
-import { ActionLinksPanel } from '../panels/action-links-panel.js';
+import { TabPanel, SnackbarList } from '@wordpress/components';
+import { SettingsPanel } from '../panels/settings-panel';
+import { HomePanel } from '../panels/home-panel';
+import { ActionLinksPanel } from '../panels/action-links-panel';
 
 const Notices = () => {
 	const notices = useSelect(
@@ -27,108 +27,36 @@ const Notices = () => {
 	);
 };
 
-const SETTINGS = {
-	DESCRIPTION_META_TAG: '_description_meta_tag',
-	SKIP_LINK: '_skip_link',
-	HEADER_FOOTER: '_header_footer',
-	PAGE_TITLE: '_page_title',
-	HELLO_PLUS_STYLE: '_hello_plus_style',
-	HELLO_PLUS_THEME: '_hello_plus_theme',
-};
-
 export const SettingsPage = () => {
-	const [ hasLoaded, setHasLoaded ] = useState( false );
-	const [ settingsData, setSettingsData ] = useState( {} );
-
-	const settingsPrefix = 'hello_plus_settings';
-
-	/**
-	 * Update settings data.
-	 *
-	 * @param {string} settingsName
-	 * @param {string} settingsValue
-	 */
-	const updateSettings = ( settingsName, settingsValue ) => {
-		setSettingsData( {
-			...settingsData,
-			[ settingsName ]: settingsValue,
-		} );
-	};
-
-	/**
-	 * Save settings to server.
-	 */
-	const saveSettings = () => {
-		const data = {};
-
-		Object.values( SETTINGS ).forEach( ( value ) => data[ `${ settingsPrefix }${ value }` ] = settingsData[ value ] ? 'true' : '' );
-
-		const settings = new api.models.Settings( data );
-
-		settings.save();
-
-		dispatch( 'core/notices' ).createNotice(
-			'success',
-			__( 'Settings Saved', 'hello-plus' ),
-			{
-				type: 'snackbar',
-				isDismissible: true,
-			},
-		);
-	};
-
-	useEffect( () => {
-		const fetchSettings = async () => {
-			try {
-				await api.loadPromise;
-				const settings = new api.models.Settings();
-				const response = await settings.fetch();
-
-				const data = {};
-				Object.values( SETTINGS ).forEach( ( value ) => data[ value ] = response[ `${ settingsPrefix }${ value }` ] );
-
-				setSettingsData( data );
-				setHasLoaded( true );
-			} catch ( error ) {
-				// eslint-disable-next-line no-console
-				console.error( error );
-			}
-		};
-
-		if ( hasLoaded ) {
-			return;
-		}
-
-		fetchSettings();
-	}, [ settingsData ] );
-
-	if ( ! hasLoaded ) {
-		return (
-			<Placeholder>
-				<Spinner />
-			</Placeholder>
-		);
-	}
+	const tabs = [
+		{
+			name: 'HOME',
+			children: <HomePanel />,
+			title: __( 'Home', 'hello-plus' ),
+			className: 'hello-plus-home-panel',
+		},
+		{
+			name: 'SETTINGS',
+			children: <SettingsPanel />,
+			title: __( 'Settings', 'hello-plus' ),
+			className: 'hello-plus-settings-panel',
+		},
+	];
 
 	return (
 		<Fragment>
 			<div className="hello_plus__header">
 				<div className="hello_plus__container">
 					<div className="hello_plus__title">
-						<h1>{ __( 'Hello Plus Theme Settings', 'hello-plus' ) }</h1>
+						<h1>{ __( 'Hello Plus Dashboard', 'hello-plus' ) }</h1>
 					</div>
 				</div>
 			</div>
 			<div className="hello_plus__main">
-				<Panel>
-
-					<SettingsPanel { ...{ SETTINGS, settingsData, updateSettings } } />
-
-					<Button isPrimary onClick={ saveSettings }>
-						{ __( 'Save Settings', 'hello-plus' ) }
-					</Button>
-
-				</Panel>
+				<TabPanel className="hello-plus-home-tabs"
+					activeClass="active-tab"
+					tabs={ tabs } >{ ( tab ) => tab.children }
+				</TabPanel>
 				<ActionLinksPanel />
 			</div>
 			<div className="hello_plus__notices">
---
+++ assets/dev/js/admin/panels/home-panel.js
@@ -0,0 +1,11 @@
+import { PanelBody } from '@wordpress/components';
+
+export const HomePanel = () => {
+	return (
+		<div>
+			<PanelBody>
+				<p>Hi, I`m just a temporary placeholder</p>
+			</PanelBody>
+		</div>
+	);
+};

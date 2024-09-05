ada81da16e2593979c1f2e7cf93a3250ede71186
Nurit <nurit@elementor.com>
2024-09-05 12:54:28 +0300

    use components

--- assets/dev/js/admin/pages/settings-page.js
+++ assets/dev/js/admin/pages/settings-page.js
@@ -2,6 +2,8 @@ import { Fragment } from 'react';
 import { __ } from '@wordpress/i18n';
 // TODO: minimize to only necessary imports
 import { Container, Box, Tabs, Tab, TabPanel, useTabs } from '@elementor/ui';
+import { ThemeProvider } from '@elementor/ui/styles';
+
 import { SettingsPanel } from '../panels/settings-panel';
 import { HomePanel } from '../panels/home-panel';
 import { ActionLinksPanel } from '../panels/action-links-panel';
@@ -27,15 +29,15 @@ export const SettingsPage = ( props ) => {
 	const { getTabsProps, getTabProps, getTabPanelProps } = useTabs( params.tab );
 
 	return (
-		<Fragment>
-			<div className="hello_plus__header">
-				<div className="hello_plus__container">
-					<div className="hello_plus__title">
+		<ThemeProvider colorScheme="auto">
+			<Box className="hello_plus__header" component="div">
+				<Box className="hello_plus__container" component="div">
+					<Box className="hello_plus__title" component="div">
 						<h1>{ __( 'Hello+ Dashboard', 'hello-plus' ) }</h1>
-					</div>
-				</div>
-			</div>
-			<div className="hello_plus__main">
+					</Box>
+				</Box>
+			</Box>
+			<Box className="hello_plus__main" component="div">
 				<Container sx={ { width: '100%' } }>
 					<Box sx={ { borderBottom: 1, borderColor: 'divider' } }>
 						<Tabs { ...props } { ...getTabsProps() }>
@@ -49,9 +51,9 @@ export const SettingsPage = ( props ) => {
 					}
 				</Container>
 				<ActionLinksPanel />
-			</div>
-			<div className="hello_plus__notices">
-			</div>
-		</Fragment>
+			</Box>
+			<Box className="hello_plus__notices" component="div">
+			</Box>
+		</ThemeProvider>
 	);
 };

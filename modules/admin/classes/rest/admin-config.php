fda358d42b5f1dd58dfde0aea8f135b8571aa8d2
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-10-06 16:56:14 +0200

    Fix admin [TMZ-67] (#27)

--- modules/admin/classes/rest/admin-config.php
+++ modules/admin/classes/rest/admin-config.php
@@ -2,6 +2,7 @@
 
 namespace HelloPlus\Modules\Admin\Classes\Rest;
 
+use HelloPlus\Includes\Utils;
 use HelloPlus\Modules\Admin\Classes\Menu\Pages\Kits_Library;
 use HelloPlus\Modules\Admin\Classes\Menu\Pages\Setup_Wizard;
 use WP_REST_Server;
@@ -63,7 +64,7 @@ class Admin_Config {
 				$last_five_pages_query->the_post();
 				$site_pages[] = [
 					'title' => get_the_title(),
-					'link' => get_edit_post_link( get_the_ID() ),
+					'link' => get_edit_post_link( get_the_ID(), 'admin' ) . '&action=elementor',
 				];
 			}
 		}
@@ -80,6 +81,7 @@ class Admin_Config {
 		];
 
 		if ( ! Setup_Wizard::has_site_wizard_been_completed() ) {
+
 			$general[] = [
 				'title' => __( 'Run setup wizard', 'hello-plus' ),
 				'link' => admin_url( 'admin.php?page=hello-plus-setup-wizard' ),
@@ -101,15 +103,51 @@ class Admin_Config {
 			'general' => $general,
 		];
 
+		$config = $this->get_quicklinks( $config );
+
+		return $config;
+	}
+
+	public function get_open_homepage_with_tab( $action ) {
+		return get_edit_post_link( get_option( 'page_on_front' ), 'admin' ) . '&action=elementor&active-document=34&tab=' . $action;
+	}
+
+	public function get_quicklinks( $config ) {
+		$customizer_title_tagline_url = admin_url( 'customize.php?autofocus[section]=title_tagline' );
+		$config['quickLinks'] = [
+			'site_name' => [
+				'title' => __( 'Site name', 'hello-plus' ),
+				'link' => Utils::is_elementor_active() ? $this->get_open_homepage_with_tab( 'settings-site-identity' ) : $customizer_title_tagline_url,
+			],
+			'site_logo' => [
+				'title' => __( 'Site Logo', 'hello-plus' ),
+				'link' => Utils::is_elementor_active() ? $this->get_open_homepage_with_tab( 'settings-site-identity' ) : $customizer_title_tagline_url,
+			],
+		];
+
+		if ( Utils::is_elementor_active() ) {
+
+			$config['quickLinks']['site_colors'] = [
+				'title' => __( 'Site Colors', 'hello-plus' ),
+				'link' => $this->get_open_homepage_with_tab( 'global-colors' ),
+			];
+
+			$config['quickLinks']['site_fonts'] = [
+				'title' => __( 'Site Fonts', 'hello-plus' ),
+				'link' => admin_url( 'admin.php?page=elementor_custom_fonts' ),
+			];
+		}
+
 		return $config;
 	}
 
 	public function get_welcome_box_config( array $config ): array {
+
 		$config['welcome'] = [
 			[
 				'title' => __( 'Edit home page', 'hello-plus' ),
 				'variant' => 'contained',
-				'link' => get_edit_post_link( get_option( 'page_on_front' ) ),
+				'link' => get_edit_post_link( get_option( 'page_on_front' ), 'admin' ) . '&action=elementor',
 			],
 			[
 				'title' => __( 'View site', 'hello-plus' ),
@@ -119,6 +157,7 @@ class Admin_Config {
 		];
 
 		if ( ! Setup_Wizard::has_site_wizard_been_completed() ) {
+
 			$config['welcome'][] = [
 				'title' => __( 'Run setup wizard', 'hello-plus' ),
 				'variant' => 'contained',

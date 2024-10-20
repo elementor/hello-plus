c208c9164f3e4a0c52a86234c75053526cb0309a
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-10-20 14:45:08 +0200

    fixes for admin [TMZ-76] (#31)

--- modules/admin/assets/js/components/top-bar/top-bar.js
+++ modules/admin/assets/js/components/top-bar/top-bar.js
@@ -1,26 +1,10 @@
 import AppBar from '@elementor/ui/AppBar';
-import HelpIcon from '@elementor/icons/HelpIcon';
 import { TopBarContent } from './top-bar-content';
-
-const adminTopBarLinks = [
-	{
-		label: 'Todo1',
-		hrefStr: '#',
-		children: <HelpIcon />,
-		aria: 'todo-1',
-	},
-	{
-		label: 'Todo2',
-		hrefStr: '#',
-		children: <HelpIcon />,
-		aria: 'todo-2',
-	},
-];
 
 export const TopBar = () => {
 	return (
 		<AppBar position="absolute" sx={ { width: 'calc(100% - 160px)', top: 0, right: 0, height: 50, backgroundColor: 'background.default' } }>
-			<TopBarContent adminTopBarLinks={ adminTopBarLinks } />
+			<TopBarContent />
 		</AppBar>
 	);
 };
--- modules/admin/assets/js/hello-plus-admin.js
+++ modules/admin/assets/js/hello-plus-admin.js
@@ -1,11 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { AdminPage } from './pages/admin-page.js';
 import { AdminProvider } from './providers/admin-provider';
+import Container from '@elementor/ui/Container';
+import Box from '@elementor/ui/Box';
 
 const App = () => {
 	return (
 		<AdminProvider>
-			<AdminPage />
+			<Box sx={ { pr: 1 } }>
+				<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', pt: { xs: 2, md: 6 }, pb: 2 } }>
+					<AdminPage />
+				</Container>
+			</Box>
 		</AdminProvider>
 	);
 };

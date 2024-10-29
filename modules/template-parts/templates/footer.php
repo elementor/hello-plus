b54b5a0624aef11806e17fc2ada85691a562704c
Nurit <nurit@elementor.com>
2024-10-29 19:57:03 +0200

    Fix documents query [TMZ-74]

--- modules/template-parts/templates/footer.php
+++ modules/template-parts/templates/footer.php
@@ -1,11 +1,12 @@
 <?php
 use HelloPlus\Modules\TemplateParts\Documents\Footer;
+use HelloPlus\Includes\Utils as Theme_Utils;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
-$footer = new Footer();
+$footer_doc_post = Footer::get_document_post();
+$footer = Theme_Utils::elementor()->documents->get( $footer_doc_post );
 $footer->print_content();
 wp_footer();
 ?>

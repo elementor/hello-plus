5386a908d61a4e19c836d232927506cb5b9cb734
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-11-06 03:30:43 +0400

    remove notice from plugin

--- modules/admin/module.php
+++ modules/admin/module.php
@@ -27,7 +27,6 @@ class Module extends Module_Base {
 	 */
 	protected function get_component_ids(): array {
 		return [
-			'Plugin_Notice',
 			'Api_Controller',
 			'Scripts_Controller',
 			'Admin_Menu_Controller',

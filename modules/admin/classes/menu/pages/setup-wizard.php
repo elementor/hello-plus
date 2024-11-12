a681108112e1f65bf3b26c7b2fa599f7ba71e36e
Nurit <nurit@elementor.com>
2024-11-12 11:42:36 +0200

    bail early from methods and use local static instead of class-level static.

--- modules/admin/classes/menu/pages/setup-wizard.php
+++ modules/admin/classes/menu/pages/setup-wizard.php
@@ -9,45 +9,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Setup_Wizard {
-
-	public static $is_setup_wizard_completed;
-
 	const SETUP_WIZARD_PAGE_SLUG = 'hello-plus-setup-wizard';
 
 	public static function has_site_wizard_been_completed(): bool {
+		static $is_setup_wizard_completed = null;
+
 		if ( ! class_exists( '\Elementor\App\Modules\ImportExport\Processes\Revert' ) ) {
 			return false;
 		}
 
-		if ( null === self::$is_setup_wizard_completed ) {
-			$sessions = \Elementor\App\Modules\ImportExport\Processes\Revert::get_import_sessions();
-
-			if ( ! $sessions ) {
-				return false;
-			}
-
-			$last_session = end( $sessions );
-			$kit_name = $last_session['kit_name'];
-
-			try {
-				/**
-				 * @var \HelloPlus\Modules\Admin\Classes\Rest\Onboarding_Settings $onboarding_rest
-				 */
-				$onboarding_rest = Module::instance()->get_component( 'Api_Controller' )->get_endpoint( 'onboarding-settings' );
-
-				$kits = $onboarding_rest->get_kits();
-
-				$kit = array_filter( $kits, function ( $k ) use ( $kit_name ) {
-					return $k['manifest']['name'] === $kit_name;
-				} );
-
-				self::$is_setup_wizard_completed = ! empty( $kit );
-			} catch ( \Exception $e ) {
-				self::$is_setup_wizard_completed = false;
-			}
+		if ( ! is_null( $is_setup_wizard_completed ) ) {
+			return $is_setup_wizard_completed;
 		}
 
-		return self::$is_setup_wizard_completed;
+		$sessions = \Elementor\App\Modules\ImportExport\Processes\Revert::get_import_sessions();
+
+		if ( ! $sessions ) {
+			return false;
+		}
+
+		$last_session = end( $sessions );
+		$kit_name = $last_session['kit_name'];
+
+		try {
+			/**
+			 * @var \HelloPlus\Modules\Admin\Classes\Rest\Onboarding_Settings $onboarding_rest
+			 */
+			$onboarding_rest = Module::instance()->get_component( 'Api_Controller' )->get_endpoint( 'onboarding-settings' );
+
+			$kits = $onboarding_rest->get_kits();
+
+			$kit = array_filter( $kits, function ( $k ) use ( $kit_name ) {
+				return $k['manifest']['name'] === $kit_name;
+			} );
+
+			$is_setup_wizard_completed = ! empty( $kit );
+		} catch ( \Exception $e ) {
+			$is_setup_wizard_completed = false;
+		}
+
+		return $is_setup_wizard_completed;
 	}
 
 	public function register_setup_wizard_page(): void {

342506380e562f64808d9abd8bdc14495bb2ac90
Nurit <nurit@elementor.com>
2024-12-01 19:28:36 +0200

    form tel input pattern

--- modules/forms/fields/tel.php
+++ modules/forms/fields/tel.php
@@ -20,7 +20,7 @@ class Tel extends Field_Base {
 
 	public function render( $item, $item_index, $form ) {
 		$form->add_render_attribute( 'input' . $item_index, 'class', 'elementor-field-textual' );
-		$form->add_render_attribute( 'input' . $item_index, 'pattern', '[0-9()#&+*-=.]+' );
+		$form->add_render_attribute( 'input' . $item_index, 'pattern', '[0-9()#&+*\-=.]+' );
 		$form->add_render_attribute( 'input' . $item_index, 'title', esc_html__( 'Only numbers and phone characters (#, -, *, etc) are accepted.', 'hello-plus' ) );
 		?>
 		<input size="1" <?php $form->print_render_attribute_string( 'input' . $item_index ); ?>>

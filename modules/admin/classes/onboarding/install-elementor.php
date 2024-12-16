a9f514a1ddc322b230b6806de418686a658a1ace
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-12-16 23:40:15 +0100

    Add logic to check for elementor version if elementor is already installed [TMZ-242]

--- modules/admin/classes/onboarding/install-elementor.php
+++ modules/admin/classes/onboarding/install-elementor.php
@@ -13,6 +13,8 @@ class Install_Elementor {
 	}
 
 	public function activate() {
+		$this->check_if_local_version_is_higher_than_min_version();
+
 		$activated = activate_plugin( 'elementor/elementor.php' );
 
 		if ( is_wp_error( $activated ) ) {
@@ -20,5 +22,30 @@ class Install_Elementor {
 		}
 
 		wp_send_json_success( [ 'message' => __( 'Elementor activated successfully.', 'hello-plus' ) ] );
+	}
+
+	public function check_if_local_version_is_higher_than_min_version(): void {
+		$plugin_file = WP_PLUGIN_DIR . '/elementor/elementor.php';
+
+		if ( file_exists( $plugin_file ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+			$plugin_data = get_plugin_data( $plugin_file );
+
+			$plugin_version = $plugin_data['Version'];
+
+			if ( version_compare( $plugin_version, HELLOPLUS_MIN_ELEMENTOR_VERSION, '<' ) ) {
+				wp_send_json_error(
+					[
+						'errorMessage' => sprintf(
+							__(
+								'Elementor plugin version needs to be at least %s for Hello Plus to Work. Please update.', 'hello-plus'
+							),
+							HELLOPLUS_MIN_ELEMENTOR_VERSION
+						),
+					],
+				);
+			}
+		}
 	}
 }

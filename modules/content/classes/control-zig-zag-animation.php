4a47a5f145aa0d099017845c3eaf88d061ed34b5
Nurit <nurit@elementor.com>
2024-12-23 15:19:50 +0200

    text domain

--- modules/content/classes/control-zig-zag-animation.php
+++ modules/content/classes/control-zig-zag-animation.php
@@ -17,11 +17,11 @@ class Control_Zig_Zag_Animation extends Control_Animation {
 
 	public static function get_animations(): array {
 		return [
-			'fadeInDown' => esc_html__( 'Down', 'elementor-pro' ),
-			'fadeInUp' => esc_html__( 'Up', 'elementor-pro' ),
-			'fadeInRight' => esc_html__( 'Right', 'elementor-pro' ),
-			'fadeInLeft' => esc_html__( 'Left', 'elementor-pro' ),
-			'zoomIn' => esc_html__( 'Zoom', 'elementor-pro' ),
+			'fadeInDown' => esc_html__( 'Down', 'hello-plus' ),
+			'fadeInUp' => esc_html__( 'Up', 'hello-plus' ),
+			'fadeInRight' => esc_html__( 'Right', 'hello-plus' ),
+			'fadeInLeft' => esc_html__( 'Left', 'hello-plus' ),
+			'zoomIn' => esc_html__( 'Zoom', 'hello-plus' ),
 		];
 	}
 }

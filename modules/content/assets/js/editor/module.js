ff7a6520fd04e33d20cda240b1cca9d567e6234a
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-01-19 13:45:30 +0100

    lint

--- modules/content/assets/js/editor/module.js
+++ modules/content/assets/js/editor/module.js
@@ -2,7 +2,6 @@ import ControlChooseItemViewImg from './controls/choose-img.js';
 
 export default class ContentModule extends elementorModules.editor.utils.Module {
 	onElementorInit() {
-		console.log( 'onElementorInit' );
 		elementor.addControlView( 'choose-img', ControlChooseItemViewImg );
 	}
 }

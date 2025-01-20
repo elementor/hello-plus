ee6769a5197e785c193e85904c8415f15071bb60
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-01-20 20:16:11 +0100

    linting

--- modules/content/classes/choose-img-control.php
+++ modules/content/classes/choose-img-control.php
@@ -20,11 +20,11 @@ class Choose_Img_Control extends Control_Choose {
 			<div class="elementor-control-input-wrapper">
 				<div class="elementor-choices elementor-choices-img">
 					<# _.each( data.options, function( options, value ) { #>
-					<input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-choose-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
-					<label class="elementor-choices-label elementor-control-unit-2 tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
-						<img src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}">
-						<span class="elementor-screen-only">{{{ options.title }}}</span>
-					</label>
+                        <input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-choose-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
+                        <label class="elementor-choices-label elementor-control-unit-2 tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
+                            <img src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}">
+                            <span class="elementor-screen-only">{{{ options.title }}}</span>
+                        </label>
 					<# } ); #>
 				</div>
 			</div>

84747c1cc2382df4f2cd7c40a37c2dbde29cd403
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-01-29 16:59:46 +0400

    linting

--- modules/content/classes/choose-img-control.php
+++ modules/content/classes/choose-img-control.php
@@ -22,24 +22,24 @@ class Choose_Img_Control extends Control_Choose {
 			<label class="elementor-control-title">{{{ data.label }}}</label>
 			<div class="elementor-control-input-wrapper">
 				<div
-                        class="elementor-choices elementor-choices-img elementor-choices-img-grid"
-                        style="grid-template-columns: repeat({{ data.columns }}, 1fr);">
+						class="elementor-choices elementor-choices-img elementor-choices-img-grid"
+						style="grid-template-columns: repeat({{ data.columns }}, 1fr);">
 
 					<# _.each( data.options, function( options, value ) { #>
-                    <div class="elementor-choices-element">
-                        <input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-choose-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
-                        <label class="elementor-choices-label elementor-control-unit-2 tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
-                            <img class="elementor-choices-image" src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}" data-hover="{{ value }}" />
-                            <span class="elementor-screen-only">{{{ options.title }}}</span>
-                        </label>
-                    </div>
+					<div class="elementor-choices-element">
+						<input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-choose-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
+						<label class="elementor-choices-label elementor-control-unit-2 tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
+							<img class="elementor-choices-image" src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}" data-hover="{{ value }}" />
+							<span class="elementor-screen-only">{{{ options.title }}}</span>
+						</label>
+					</div>
 					<# } ); #>
 				</div>
 			</div>
 		</div>
 
 		<# if ( data.description ) { #>
-		    <div class="elementor-control-field-description">{{{ data.description }}}</div>
+			<div class="elementor-control-field-description">{{{ data.description }}}</div>
 		<# } #>
 		<?php
 	}

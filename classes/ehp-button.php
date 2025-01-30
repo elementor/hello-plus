8722d75e5acee71518b90a96cfe879315eb14d12
Nurit <nurit@elementor.com>
2025-01-30 20:37:57 +0200

    lint

--- classes/ehp-button.php
+++ classes/ehp-button.php
@@ -103,10 +103,10 @@ class Ehp_Button {
 	/**
 	 * @return mixed
 	 */
-	protected function get_control_value( string $defaults_key, $default, ?string $settings_key = null ) {
+	protected function get_control_value( string $defaults_key, $default_value, ?string $settings_key = null ) {
 		$type = ! empty( $this->context['type'] ) ? $this->context['type'] . '_' : '';
 		$settings_key = $type . ( $settings_key ?? $defaults_key );
-  		return $this->defaults[ $defaults_key ] ?? $this->widget_settings[ $settings_key ] ?? $default;
+		return $this->defaults[ $defaults_key ] ?? $this->widget_settings[ $settings_key ] ?? $default_value;
 	}
 
 	public function add_content_section() {

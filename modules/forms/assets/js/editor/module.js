b144e79153739ad5cbb86fa3144cd819359ac88e
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-03-03 19:33:39 +0100

    add control

--- modules/forms/assets/js/editor/module.js
+++ modules/forms/assets/js/editor/module.js
@@ -1,3 +1,4 @@
+import { __ } from '@wordpress/i18n';
 import Component from './component';
 import FieldsMapControl from './fields-map-control';
 import FieldsRepeaterControl from './fields-repeater-control';
@@ -19,6 +20,40 @@ export default class FormsModule extends elementorModules.editor.utils.Module {
 
 		elementor.addControlView( 'Fields_map', FieldsMapControl );
 		elementor.addControlView( 'form-fields-repeater', FieldsRepeaterControl );
+
+		elementorPromotionsData.collect_submit = {
+			title: __( 'Collect Submissions', 'hello-plus' ),
+			description: [ __( 'Upgrade to Pro "Advanced Solo" to access and manage all your form submissions in one place..', 'hello-plus' ) ],
+			upgrade_text: __( 'Upgrade', 'hello-plus' ),
+			upgrade_url: 'https://go.elementor.com/go-pro-button-widget-control/',
+			image: 'https://assets.elementor.com/free-to-pro-upsell/v1/images/cta.jpg',
+			image_alt: 'Upgrade',
+		};
+	}
+
+	showPromotion( targetElement ) {
+		const hasProAndNotConnected = elementor.helpers.hasProAndNotConnected(),
+			dialogOptions = {
+				title: __( 'Dynamic Content', 'elementor' ),
+				content: __(
+					'Create more personalized and dynamic sites by populating data from various sources with dozens of dynamic tags to choose from.',
+					'elementor',
+				),
+				targetElement,
+				position: {
+					blockStart: '-10',
+				},
+				actionButton: {
+					url: hasProAndNotConnected
+						? elementorProEditorConfig.urls.connect
+						: elementor.config.dynamicPromotionURL.replace( '%s', this.view.model.get( 'name' ) ),
+					text: hasProAndNotConnected
+						? __( 'Connect & Activate', 'elementor' )
+						: __( 'Upgrade', 'elementor' ),
+				},
+			};
+
+		elementor.promotion.showDialog( dialogOptions );
 	}
 
 	onElementorInitComponents() {

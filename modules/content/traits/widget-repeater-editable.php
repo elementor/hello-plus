dd3c8bde0f073306d8aa905fb03e91d724f54675
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-04-01 18:07:06 +0800

    linting

--- modules/content/traits/widget-repeater-editable.php
+++ modules/content/traits/widget-repeater-editable.php
@@ -8,8 +8,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 trait Widget_Repeater_Editable {
 
-	public function public_add_inline_editing_attributes( $attribute, $default = 'basic' ) {
-		return $this->add_inline_editing_attributes( $attribute, $default );
+	public function public_add_inline_editing_attributes( $attribute, $toolbar = 'basic' ) {
+		return $this->add_inline_editing_attributes( $attribute, $toolbar );
 	}
 
 	public function public_get_repeater_setting_key( $setting_key, $repeater_key, $item_key ) {

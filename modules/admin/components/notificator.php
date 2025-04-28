8d28b0e5b75c9d8ce1a8d8bc76da1222184fac8c
Nurit <nurit@elementor.com>
2025-04-28 17:36:13 +0300

    Tweak: Use Notifications SDK 1.2.0 [TMZ-535]

--- modules/admin/components/notificator.php
+++ modules/admin/components/notificator.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-use Elementor\WPNotificationsPackage\V110\Notifications as Notifications_SDK;
+use Elementor\WPNotificationsPackage\V120\Notifications as Notifications_SDK;
 
 class Notificator {
 	private ?Notifications_SDK $notificator = null;
@@ -19,10 +19,13 @@ class Notificator {
 			require_once HELLOPLUS_PATH . '/vendor/autoload.php';
 		}
 
-		$this->notificator = new Notifications_SDK(
-			'hello-plus',
-			HELLOPLUS_VERSION,
-			'ehp'
-		);
+		$this->notificator = new Notifications_SDK( [
+			'app_name' => 'hello-plus',
+			'app_version' => HELLOPLUS_VERSION,
+			'short_app_name' => 'ehp',
+			'app_data' => [
+				'plugin_basename' => HELLOPLUS_PLUGIN_BASE,
+			],
+		] );
 	}
 }

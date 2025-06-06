2727d04456badb7ff9358bdfba06207fe9769b4b
Giorgos Sarigiannidis <contact@gsarigiannidis.gr>
2025-06-06 18:50:23 +0300

    Adds the logic to handle the scenario where no part exists

--- modules/theme/components/theme-overrides.php
+++ modules/theme/components/theme-overrides.php
@@ -151,8 +151,12 @@ class Theme_Overrides {
 			if ( ! isset( $part['id'] ) || ! in_array( $part['id'], [ 'header', 'footer' ], true ) || false === $part['showSublinks'] ) {
 				continue;
 			}
-
-			$this->update_site_part_link( $part, $part['id'] );
+			$active_document = $this->get_active_document_by_part_type( $part['id'] );
+			if ( isset( $part['showSublinks'] ) && empty( $active_document ) ) {
+				$part['link'] = $this->get_add_new_part_link( $part['id'] );
+			} else {
+				$this->update_site_part_link( $part, $part['id'] );
+			}
 		}
 
 		return $site_parts;

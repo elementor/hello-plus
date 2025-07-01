b31969f7010c428b844d7de61d0691278af7c568
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-07-01 15:12:26 +0200

    Internal: Allow widget editing on elementor domain [TMZ-39]

--- modules/template-parts/assets/js/editor/module.js
+++ modules/template-parts/assets/js/editor/module.js
@@ -2,15 +2,15 @@ import Component from './component';
 
 export default class TemplatesModule extends elementorModules.editor.utils.Module {
 	onElementorInit() {
-		$e.components.register( new Component( { manager: this } ) );
-		elementor.channels.editor.on( 'helloPlusLogo:change', this.openSiteIdentity );
-		elementor.hooks.addFilter( 'elements/widget/controls/common/default', this.resetCommonControls.bind( this ) );
-		elementor.hooks.addFilter( 'elements/widget/controls/common-optimized/default', this.resetCommonControls.bind( this ) );
-		elementor.hooks.addFilter( 'templates/source/is-remote', this.setSourceAsRemote.bind( this ) );
-		elementor.hooks.addFilter( 'elements/base/behaviors', this.filterBehviors.bind( this ), 1000 );
+		$e.components.register(new Component({ manager: this }));
+		elementor.channels.editor.on('helloPlusLogo:change', this.openSiteIdentity);
+		elementor.hooks.addFilter('elements/widget/controls/common/default', this.resetCommonControls.bind(this));
+		elementor.hooks.addFilter('elements/widget/controls/common-optimized/default', this.resetCommonControls.bind(this));
+		elementor.hooks.addFilter('templates/source/is-remote', this.setSourceAsRemote.bind(this));
+		elementor.hooks.addFilter('elements/base/behaviors', this.filterBehviors.bind(this), 1000);
 		elementor.hooks.addFilter(
 			'component/modal/close',
-			this.preventClosingModal.bind( this ),
+			this.preventClosingModal.bind(this),
 			1000,
 		);
 
@@ -19,39 +19,39 @@ export default class TemplatesModule extends elementorModules.editor.utils.Modul
 			'core/modal/close/ehp-header',
 		];
 
-		types.forEach( ( type ) => {
-			window.addEventListener( type, this.redirectToHelloPlus );
-		} );
+		types.forEach((type) => {
+			window.addEventListener(type, this.redirectToHelloPlus);
+		});
 
 		window.templatesModule = this;
 	}
 
-	preventClosingModal( close, component ) {
+	preventClosingModal(close, component) {
 		if (
 			'library' === component.getNamespace() &&
 			'library/templates/ehp-elements' === component?.defaultRoute
 		) {
-			return () => {};
+			return () => { };
 		}
 		return close;
 	}
 
-	filterBehviors( behaviors ) {
-		if ( this.isEhpDocument() && this.notElementorDomain() ) {
+	filterBehviors(behaviors) {
+		if (this.isEhpDocument() && this.notElementorDomain()) {
 			const { contextMenu: { groups } } = behaviors;
 			behaviors.contextMenu.groups = groups
-				.map( this.filterOutUnsupportedActions() )
-				.filter( ( group ) => group.actions.length );
+				.map(this.filterOutUnsupportedActions())
+				.filter((group) => group.actions.length);
 		}
 		return behaviors;
 	}
 
 	notElementorDomain() {
-		return ! ehpTemplatePartsEditorSettings.isElementorDomain;
+		return !ehpTemplatePartsEditorSettings.isElementorDomain;
 	}
 
-	setSourceAsRemote( isRemote, activeSource ) {
-		if ( 'remote-ehp' === activeSource ) {
+	setSourceAsRemote(isRemote, activeSource) {
+		if ('remote-ehp' === activeSource) {
 			return true;
 		}
 
@@ -59,17 +59,19 @@ export default class TemplatesModule extends elementorModules.editor.utils.Modul
 	}
 
 	redirectToHelloPlus() {
-		$e.internal( 'document/save/set-is-modified', { status: false } );
-		window.location.href = elementor.config.close_modal_redirect_hello_plus + elementor.config.document.type;
+		if (this.notElementorDomain()) {
+			$e.internal('document/save/set-is-modified', { status: false });
+			window.location.href = elementor.config.close_modal_redirect_hello_plus + elementor.config.document.type;
+		}
 	}
 
 	async openSiteIdentity() {
-		await $e.run( 'panel/global/open' );
-		$e.route( 'panel/global/settings-site-identity' );
+		await $e.run('panel/global/open');
+		$e.route('panel/global/settings-site-identity');
 	}
 
-	resetCommonControls( commonControls, widgetType ) {
-		if ( [ 'ehp-footer', 'ehp-header' ].includes( widgetType ) ) {
+	resetCommonControls(commonControls, widgetType) {
+		if (['ehp-footer', 'ehp-header'].includes(widgetType)) {
 			return null;
 		}
 
@@ -77,20 +79,20 @@ export default class TemplatesModule extends elementorModules.editor.utils.Modul
 	}
 
 	filterOutUnsupportedActions() {
-		return ( group ) => {
+		return (group) => {
 			const enabledCommands = elementor.helpers.hasPro()
-				? [ 'edit', 'delete', 'resetStyle' ]
-				: [ 'edit', 'delete', 'resetStyle', 'save' ];
+				? ['edit', 'delete', 'resetStyle']
+				: ['edit', 'delete', 'resetStyle', 'save'];
 			const { name, actions } = group;
 
 			return {
 				name,
-				actions: actions.filter( ( action ) => enabledCommands.includes( action.name ) ),
+				actions: actions.filter((action) => enabledCommands.includes(action.name)),
 			};
 		};
 	}
 
 	isEhpDocument() {
-		return [ 'ehp-footer', 'ehp-header' ].includes( elementor.config.document.type );
+		return ['ehp-footer', 'ehp-header'].includes(elementor.config.document.type);
 	}
 }

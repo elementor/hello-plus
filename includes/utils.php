f734145e6e60238ec75fc5391a014baa388ffa15
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-07-10 11:45:12 +0200

    linting

--- includes/utils.php
+++ includes/utils.php
@@ -29,8 +29,8 @@ class Utils {
 
 		foreach ( $allowed_domains as $domain ) {
 			if ( str_ends_with( $current_domain, $domain ) ) {
-                                $is_elementor_domain = true;
-                                break;
+								$is_elementor_domain = true;
+								break;
 			}
 		}
 

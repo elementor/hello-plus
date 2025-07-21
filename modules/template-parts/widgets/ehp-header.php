45d83c60247d29cc907ce41cbcbecaeabab804da
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-07-21 15:09:00 +0200

    fix

--- modules/template-parts/widgets/ehp-header.php
+++ modules/template-parts/widgets/ehp-header.php
@@ -37,8 +37,6 @@ class Ehp_Header extends Ehp_Widget_Base {
 
 	use Widget_Repeater_Editable;
 
-	public $render_strategy;
-
 	public function get_name(): string {
 		return 'ehp-header';
 	}
@@ -86,13 +84,13 @@ class Ehp_Header extends Ehp_Widget_Base {
 		);
 	}
 
-	public function render(): void {
-		$this->render_strategy = new Widget_Header_Render( $this );
+	protected function render(): void {
+		$render_strategy = new Widget_Header_Render( $this );
 
 		$this->add_inline_editing_attributes( 'primary_cta_button_text', 'none' );
 		$this->add_inline_editing_attributes( 'secondary_cta_button_text', 'none' );
 
-		$this->render_strategy->render();
+		$render_strategy->render();
 	}
 
 	protected function register_controls() {
